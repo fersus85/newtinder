aa67e28c02f266dd362b643913d5bb8bda50fccb
fersus <deriabin_85@mail.ru>
2023-06-30 18:27:36 +0300

    update method save, where i add watermark on user image

--- newtinder/models.py
+++ newtinder/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -11,4 +12,22 @@ class Profile(AbstractUser):
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     gender = models.CharField(max_length=10, choices=choices, default='male')
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    avatar = models.ImageField(upload_to="static/images/avatars/", null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        with Image.open(self.avatar.path) as img:
+
+            width, height = img.size
+            logo = Image.open("static/images/logo.jpg")
+            size = (100, 100)
+            logo.thumbnail(size)
+
+            x = 0
+            y = 0
+
+            x = width - 100
+            y = height - 100
+
+            img.paste(logo, (x, y))
+            img.save(self.avatar.path)
